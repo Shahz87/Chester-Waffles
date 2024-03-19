2650aebc4a428ec44bf70be53becfc3cd07898a0
Shahz87 <164032185+Shahz87@users.noreply.github.com>
2024-03-19 14:15:59 +0000

    added new pages, added a picture in assests folder

---
+++ Pages/Product.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chester_waffles.Pages
+{
+    public class ProductModel : PageModel
+    {
+        private readonly ILogger<ProductModel> _logger;
+
+        public ProductModel(ILogger<ProductModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
